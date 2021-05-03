2d961444fc28684fbbae9fe2f49439c2b25aff35
abekoh <abekoh.bcky@gmail.com>
2021-05-03 22:07:15 +0900

    remove unused jackson config

--- src/main/java/dev/abekoh/warmup/config/JacksonConfig.java
+++ src/main/java/dev/abekoh/warmup/config/JacksonConfig.java
@@ -3,13 +3,9 @@ package dev.abekoh.warmup.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 
 @Configuration
 public class JacksonConfig {
@@ -22,26 +18,5 @@ public class JacksonConfig {
             .visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
             .visibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE)
             .visibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE);
-  }
-
-  @Bean
-  Jackson2JsonEncoder jackson2JsonEncoder(ObjectMapper mapper) {
-    return new Jackson2JsonEncoder(mapper);
-  }
-
-  @Bean
-  Jackson2JsonDecoder jackson2JsonDecoder(ObjectMapper mapper) {
-    return new Jackson2JsonDecoder(mapper);
-  }
-
-  @Bean
-  ExchangeStrategies exchangeStrategies(Jackson2JsonEncoder encoder, Jackson2JsonDecoder decoder) {
-    return ExchangeStrategies.builder()
-        .codecs(
-            clientDefaultCodecsConfigurer -> {
-              clientDefaultCodecsConfigurer.defaultCodecs().jackson2JsonEncoder(encoder);
-              clientDefaultCodecsConfigurer.defaultCodecs().jackson2JsonDecoder(decoder);
-            })
-        .build();
   }
 }

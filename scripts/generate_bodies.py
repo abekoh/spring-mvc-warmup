b263e52919e1b70892df13e9594433cf6ee6aead
abekoh <abekoh.bcky@gmail.com>
2021-05-03 18:16:04 +0900

    add script

---
+++ scripts/generate_bodies.py
@@ -0,0 +1,44 @@
+import requests
+from datetime import datetime
+import random
+import json
+import base64
+import argparse
+
+
+def generate_name_list(url):
+    resp = requests.get(url)
+    return tuple(
+        filter(lambda x: x != "", [line.rstrip("\r") for line in resp.text.split("\n")])
+    )
+
+
+LAST_NAME_LIST = generate_name_list(
+    "https://raw.githubusercontent.com/smashew/NameDatabases/master/NamesDatabases/surnames/us.txt"
+)
+FIRST_NAME_LIST = generate_name_list(
+    "https://raw.githubusercontent.com/smashew/NameDatabases/master/NamesDatabases/first%20names/us.txt"
+)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dummy", help="with dummy request", action="store_true")
+    args = parser.parse_args()
+    while True:
+        result = {}
+        result["method"] = "POST"
+        result["url"] = "http://localhost:8080/api/users"
+        # 1970-01-01 - 2020-12-31
+        rand_date = datetime.fromtimestamp(random.randint(0, 1609426799))
+        body_dic = {
+            "firstName": random.choice(FIRST_NAME_LIST),
+            "lastName": random.choice(LAST_NAME_LIST),
+            "birthYear": rand_date.year,
+            "birthMonth": rand_date.month,
+            "birthDate": rand_date.day,
+            "isDummy": args.dummy,
+        }
+        result["header"] = {"Content-Type": ["application/json"]}
+        result["body"] = base64.b64encode(json.dumps(body_dic).encode()).decode()
+        print(json.dumps(result))

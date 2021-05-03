c65470eaa28f0d3459c0e59cce0619e59b00ce11
abekoh <abekoh.bcky@gmail.com>
2021-05-03 22:23:38 +0900

    update profile

--- src/test/java/dev/abekoh/warmup/WarmupSandboxApplicationTests.java
+++ src/test/java/dev/abekoh/warmup/WarmupSandboxApplicationTests.java
@@ -2,8 +2,10 @@ package dev.abekoh.warmup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class WarmupSandboxApplicationTests {
 
   @Test
